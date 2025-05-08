0c9b2a975dbe09b5fdc1d2a98c33ef0a45818a20
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2025-05-09 01:01:58 +0300

    add data base manager
    persistent container and CRUD methods

--- ToDoListTest/DateBaseManager/DataBaseManager.swift
+++ ToDoListTest/DateBaseManager/DataBaseManager.swift
@@ -6,3 +6,75 @@
 //
 
 import Foundation
+import CoreData
+
+final class DataBaseManager {
+    
+    static let shared = DataBaseManager()
+   
+    private init() { } //  func fetchNotes in init
+    var notes: [Note] = []
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer (name: "Note")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save ()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(error), \(nserror.userInfo)")
+            }
+        }
+    }
+}
+
+//MARK: - CRUD
+extension DataBaseManager {
+    
+    func createNote(title: String, text: String) {
+        let note = Note(context: persistentContainer.viewContext)
+        note.title = title
+        note.text = text
+        note.date = Date.now
+        note.completed = false
+        saveContext()
+    }
+    
+    func fetchNotes() {
+        let request = Note.fetchRequest()
+        request.sortDescriptors = [NSSortDescriptor(key: "date", ascending: true)]
+        do {
+            let notes = try persistentContainer.viewContext.fetch(request)
+            self.notes = notes
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func updateNote(note: Note, newTitle: String = "", newText: String = "") {
+        note.title = newTitle
+        note.text = newText
+        saveContext()
+    }
+    
+    func deleteNote(note: Note) {
+        let context = persistentContainer.viewContext
+        context.delete(note)
+        saveContext()
+    }
+}
+
+                                       
+
+    
+    
