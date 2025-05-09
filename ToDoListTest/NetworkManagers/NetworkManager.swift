8b6f46dad1585a35d8bf5361ee8b14e76983a46d
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2025-05-10 02:16:21 +0300

    add network manager for fetch notes

--- ToDoListTest/NetworkManagers/NetworkManager.swift
+++ ToDoListTest/NetworkManagers/NetworkManager.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import SwiftUI
+
+class NetworkManager {
+    let link: String = "https://dummyjson.com/todos"
+    var apiNotes: [ApiNote] = []
+    
+    init() { fetchNotes() }
+    func fetchNotes() {
+        guard let url = URL(string: link) else { print("bad URL error"); return }
+        
+        URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+            guard error == nil else { print("request error"); return }
+            guard let data = data else { print("no data"); return }
+            
+            do {
+                let notes = try JSONDecoder().decode(ApiNotes.self, from: data)
+                DispatchQueue.main.async {
+                    print("Api notes --- >>>",notes.todos)
+                    self.apiNotes = notes.todos
+                }
+            } catch {
+                print("decode error")
+            }
+        }.resume()
+    }
+}
