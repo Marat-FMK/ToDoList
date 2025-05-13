b9f1617705f163de076c215c6d28895932940a6f
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2025-05-14 00:54:35 +0300

    add editing view

--- ToDoListTest/Views/Home/HomeView.swift
+++ ToDoListTest/Views/Home/HomeView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct HomeView: View {
     @StateObject var viewModel = HomeViewModel()
+    @Environment(\.managedObjectContext) private var context
     @FetchRequest(
         entity: Note.entity(),
         sortDescriptors: [NSSortDescriptor(keyPath: \Note.date, ascending: false)]
@@ -16,32 +17,42 @@ struct HomeView: View {
     
     var body: some View {
         NavigationStack {
-            VStack {
-                VStack(alignment: .leading) {
-                    Text("Задачи")
-                        .foregroundStyle(.appText)
-                        .font(.system(size: 40))
-                        .bold()
-                    
-                    SearchTextField(searchText: $viewModel.searchText, search: viewModel.searchNote, activateRecord: viewModel.startRecodr, clearSearchText: viewModel.clearSearchText)
-                    
-                    ScrollView {
-                        if viewModel.searchedNotes.isEmpty && viewModel.searchText.isEmpty {
-                            ForEach(notes) { note in
-                                NoteCell(note: note, updateNoteStatus: viewModel.updateNoteStatus)
-                            }
-                        } else {
-                            ForEach(viewModel.searchedNotes) { note in
-                                NoteCell(note: note, updateNoteStatus: viewModel.updateNoteStatus)
-                            }
-                        }
+            ZStack {
+                if viewModel.editing {
+                    if let note = viewModel.selectedNote {
+                        NoteEditing(note: note, deleteNote: viewModel.deleteNote)
+                            .zIndex(1)
                     }
                 }
-                .padding(.horizontal, 20)
+                    VStack {
+                        VStack(alignment: .leading) {
+                            Text("Задачи")
+                                .foregroundStyle(.appText)
+                                .font(.system(size: 40))
+                                .bold()
+                            
+                            SearchTextField(searchText: $viewModel.searchText, search: viewModel.searchNote, activateRecord: viewModel.startRecodr, clearSearchText: viewModel.clearSearchText)
+                            
+                            ScrollView {
+                                if viewModel.searchedNotes.isEmpty && viewModel.searchText.isEmpty {
+                                    ForEach(notes) { note in
+                                        NoteCell(note: note, updateNoteStatus: viewModel.updateNoteStatus, selectNote: viewModel.selectNote)
+                                    }
+                                } else {
+                                    ForEach(viewModel.searchedNotes) { note in
+                                        NoteCell(note: note, updateNoteStatus: viewModel.updateNoteStatus, selectNote: viewModel.selectNote)
+                                    }
+                                }
+                            }
+                        }
+                        .padding(.horizontal, 20)
+                        
+                        BottomBar(notesCount: notes.count)
+                    }
+                    .background(.appBackground)
+                    .blur(radius: CGFloat(viewModel.blurValue))
                 
-                BottomBar(notesCount: notes.count)
             }
-            .background(.appBackground)
         }
     }
 }
