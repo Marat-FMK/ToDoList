edd932dc43397c45ec002253c4171aa31a8a9bf2
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2025-05-14 00:55:33 +0300

    editing functional // add custom buttons

--- ToDoListTest/Views/Home/NoteEditing.swift
+++ ToDoListTest/Views/Home/NoteEditing.swift
@@ -8,11 +8,32 @@
 import SwiftUI
 
 struct NoteEditing: View {
+    @ObservedObject var note: Note
+    let deleteNote: (Note) -> Void
+//    let cancelingSelection: () -> Void
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            VStack(alignment: .leading, spacing: 10) {
+                Text(note.title)
+                    .foregroundStyle(.appText)
+                    .bold()
+                Text(note.text)
+                    .foregroundStyle(.appText)
+                    .font(.system(size: 15))
+                    .lineLimit(2)
+                Text(note.date?.formatted() ?? "no date")
+                    .foregroundStyle(.appDate)
+            }
+            .padding(15)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .background {
+                RoundedRectangle(cornerRadius: 16)
+                    .foregroundStyle(.appBottomBar)
+            }
+            .padding(20)
+            
+            NoteEditingButtons(note: note, deleteNote: deleteNote)
+        }
     }
 }
 
-#Preview {
-    NoteEditing()
-}
--- ToDoListTest/Views/Home/NoteEditingButtons.swift
+++ ToDoListTest/Views/Home/NoteEditingButtons.swift
@@ -7,12 +7,36 @@
 
 import SwiftUI
 
-struct NoteEditingButtons: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+import SwiftUI
 
-#Preview {
-    NoteEditingButtons()
+struct NoteEditingButtons: View {
+    @ObservedObject var note: Note
+    let deleteNote: (Note) -> Void
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            NavigationLink {
+                DetailView(note: note)
+            } label: {
+                EditingButtonLabel(title: "Редактировать", imageName: "square.and.pencil", color: .black)
+            }
+            
+            Divider()
+            ShareLink( item: "Задача - \(note.title),  \(note.text)") {
+                EditingButtonLabel(title: "Поделиться", imageName: "square.and.arrow.up", color: .black)
+            }
+            Divider()
+            
+            Button {
+                deleteNote(note)
+            } label: {
+                EditingButtonLabel(title: "Удалить", imageName: "trash", color: .red)
+            }
+        }
+        .padding(20)
+        .background {
+            RoundedRectangle(cornerRadius: 16).foregroundStyle(.appSearchItems)
+        }
+        .padding(.horizontal, 50)
+    }
 }
